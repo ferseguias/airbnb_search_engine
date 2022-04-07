6e5b388c737cb8d72fab9fd50516573565bce814
ferseguias <ferseg@gmail.com>
2022-04-08 01:30:11 +0200

    mas tablas sql cargadas

--- sql_files/create_tables_airbnb.sql
+++ sql_files/create_tables_airbnb.sql
@@ -12,6 +12,14 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema airbnb
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `airbnb` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema publications
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema publications
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `publications` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `airbnb` ;
 
 -- -----------------------------------------------------
@@ -27,13 +35,14 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `airbnb`.`host`
+-- Table `airbnb`.`hosts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `airbnb`.`host` (
-  `idhost` INT NOT NULL,
-  `host_since` DATETIME NULL,
-  `host_is_superhost` TINYINT NULL,
-  PRIMARY KEY (`idhost`))
+CREATE TABLE IF NOT EXISTS `airbnb`.`hosts` (
+  `idhosts` INT NOT NULL,
+  `host_since` TEXT(10) NULL,
+  `host_is_superhost` TEXT(1) NULL,
+  `host_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`idhosts`))
 ENGINE = InnoDB;
 
 
@@ -42,8 +51,6 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `airbnb`.`listings` (
   `idlistings` INT NOT NULL,
-  `host_idhost` INT NOT NULL,
-  `name` VARCHAR(145) NULL,
   `description` VARCHAR(1500) NULL,
   `room_type` VARCHAR(45) NOT NULL,
   `guests` INT NOT NULL,
@@ -52,20 +59,21 @@ CREATE TABLE IF NOT EXISTS `airbnb`.`listings` (
   `number_of_reviews` INT NOT NULL,
   `review_scores` FLOAT NULL,
   `geopoint` POINT NOT NULL,
-  `neighborhoods_idneighborhoods` INT NOT NULL,
   `picture_url` VARCHAR(145) NULL,
   `listing_url` VARCHAR(145) NULL,
+  `host_idhost` INT NOT NULL,
+  `neighborhoods_idneighborhoods` INT NOT NULL,
   PRIMARY KEY (`idlistings`),
-  INDEX `fk_listings_neighborhoods_idx` (`neighborhoods_idneighborhoods` ASC) VISIBLE,
   INDEX `fk_listings_host1_idx` (`host_idhost` ASC) VISIBLE,
-  CONSTRAINT `fk_listings_neighborhoods`
-    FOREIGN KEY (`neighborhoods_idneighborhoods`)
-    REFERENCES `airbnb`.`neighborhoods` (`idneighborhoods`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_listings_neighborhoods1_idx` (`neighborhoods_idneighborhoods` ASC) VISIBLE,
   CONSTRAINT `fk_listings_host1`
     FOREIGN KEY (`host_idhost`)
-    REFERENCES `airbnb`.`host` (`idhost`)
+    REFERENCES `airbnb`.`hosts` (`idhosts`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_listings_neighborhoods1`
+    FOREIGN KEY (`neighborhoods_idneighborhoods`)
+    REFERENCES `airbnb`.`neighborhoods` (`idneighborhoods`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -76,11 +84,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `airbnb`.`reviews` (
   `idreviews` INT NOT NULL,
-  `listings_idlistings` INT NOT NULL,
   `idreviewer` INT NOT NULL,
-  `date` DATETIME NOT NULL,
+  `date` TEXT(10) NOT NULL,
   `reviewer_name` VARCHAR(145) NULL,
-  `comments` VARCHAR(1000) NOT NULL,
+  `comments` TEXT(1000000) NOT NULL,
+  `listings_idlistings` INT NOT NULL,
   PRIMARY KEY (`idreviews`),
   INDEX `fk_reviews_listings1_idx` (`listings_idlistings` ASC) VISIBLE,
   CONSTRAINT `fk_reviews_listings1`
@@ -110,6 +118,247 @@ CREATE TABLE IF NOT EXISTS `airbnb`.`calendar` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+USE `publications` ;
+
+-- -----------------------------------------------------
+-- Table `publications`.`authors`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`authors` (
+  `au_id` VARCHAR(11) NOT NULL,
+  `au_lname` VARCHAR(40) NOT NULL,
+  `au_fname` VARCHAR(20) NOT NULL,
+  `phone` CHAR(12) NOT NULL,
+  `address` VARCHAR(40) NULL DEFAULT NULL,
+  `city` VARCHAR(20) NULL DEFAULT NULL,
+  `state` CHAR(2) NULL DEFAULT NULL,
+  `zip` CHAR(5) NULL DEFAULT NULL,
+  `contract` TINYINT NOT NULL,
+  PRIMARY KEY (`au_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`stores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`stores` (
+  `stor_id` CHAR(4) NOT NULL,
+  `stor_name` VARCHAR(40) NULL DEFAULT NULL,
+  `stor_address` VARCHAR(40) NULL DEFAULT NULL,
+  `city` VARCHAR(20) NULL DEFAULT NULL,
+  `state` CHAR(2) NULL DEFAULT NULL,
+  `zip` CHAR(5) NULL DEFAULT NULL,
+  PRIMARY KEY (`stor_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`discounts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`discounts` (
+  `discounttype` VARCHAR(40) NOT NULL,
+  `stor_id` CHAR(4) NULL DEFAULT NULL,
+  `lowqty` SMALLINT NULL DEFAULT NULL,
+  `highqty` SMALLINT NULL DEFAULT NULL,
+  `discount` DECIMAL(4,2) NOT NULL,
+  INDEX `discounts_stor_id` (`stor_id` ASC) VISIBLE,
+  CONSTRAINT `discounts_ibfk_1`
+    FOREIGN KEY (`stor_id`)
+    REFERENCES `publications`.`stores` (`stor_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`jobs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`jobs` (
+  `job_id` SMALLINT NOT NULL,
+  `job_desc` VARCHAR(50) NOT NULL,
+  `min_lvl` SMALLINT NOT NULL,
+  `max_lvl` SMALLINT NOT NULL,
+  PRIMARY KEY (`job_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`publishers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`publishers` (
+  `pub_id` CHAR(4) NOT NULL,
+  `pub_name` VARCHAR(40) NULL DEFAULT NULL,
+  `city` VARCHAR(20) NULL DEFAULT NULL,
+  `state` CHAR(2) NULL DEFAULT NULL,
+  `country` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`pub_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`employee` (
+  `emp_id` CHAR(9) NOT NULL,
+  `fname` VARCHAR(20) NOT NULL,
+  `minit` CHAR(1) NULL DEFAULT NULL,
+  `lname` VARCHAR(30) NOT NULL,
+  `job_id` SMALLINT NOT NULL,
+  `job_lvl` SMALLINT NULL DEFAULT NULL,
+  `pub_id` CHAR(4) NOT NULL,
+  `hire_date` DATETIME NOT NULL,
+  PRIMARY KEY (`emp_id`),
+  INDEX `employee_job_id` (`job_id` ASC) VISIBLE,
+  INDEX `employee_pub_id` (`pub_id` ASC) VISIBLE,
+  CONSTRAINT `employee_ibfk_1`
+    FOREIGN KEY (`job_id`)
+    REFERENCES `publications`.`jobs` (`job_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `employee_ibfk_2`
+    FOREIGN KEY (`pub_id`)
+    REFERENCES `publications`.`publishers` (`pub_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`most_profiting_authors`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`most_profiting_authors` (
+  `au_id` VARCHAR(11) CHARACTER SET 'utf8' NOT NULL,
+  `total_profit` DECIMAL(65,12) NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`most_profiting_authors_temp`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`most_profiting_authors_temp` (
+  `au_id` VARCHAR(11) CHARACTER SET 'utf8' NOT NULL,
+  `total_profit` DECIMAL(65,12) NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`pub_info`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`pub_info` (
+  `pub_id` CHAR(4) NOT NULL,
+  `logo` LONGBLOB NULL DEFAULT NULL,
+  `pr_info` LONGTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`pub_id`),
+  CONSTRAINT `pub_info_ibfk_1`
+    FOREIGN KEY (`pub_id`)
+    REFERENCES `publications`.`publishers` (`pub_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`titles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`titles` (
+  `title_id` VARCHAR(6) NOT NULL,
+  `title` VARCHAR(80) NOT NULL,
+  `type` CHAR(12) NOT NULL,
+  `pub_id` CHAR(4) NULL DEFAULT NULL,
+  `price` DECIMAL(19,4) NULL DEFAULT NULL,
+  `advance` DECIMAL(19,4) NULL DEFAULT NULL,
+  `royalty` INT NULL DEFAULT NULL,
+  `ytd_sales` INT NULL DEFAULT NULL,
+  `notes` VARCHAR(200) NULL DEFAULT NULL,
+  `pubdate` DATETIME NOT NULL,
+  PRIMARY KEY (`title_id`),
+  INDEX `titles_pub_id` (`pub_id` ASC) VISIBLE,
+  CONSTRAINT `titles_ibfk_1`
+    FOREIGN KEY (`pub_id`)
+    REFERENCES `publications`.`publishers` (`pub_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`roysched`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`roysched` (
+  `title_id` VARCHAR(6) NOT NULL,
+  `lorange` INT NULL DEFAULT NULL,
+  `hirange` INT NULL DEFAULT NULL,
+  `royalty` INT NULL DEFAULT NULL,
+  INDEX `roysched_title_id` (`title_id` ASC) VISIBLE,
+  CONSTRAINT `roysched_ibfk_1`
+    FOREIGN KEY (`title_id`)
+    REFERENCES `publications`.`titles` (`title_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`sales`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`sales` (
+  `stor_id` CHAR(4) NOT NULL,
+  `ord_num` VARCHAR(20) NOT NULL,
+  `ord_date` DATETIME NOT NULL,
+  `qty` SMALLINT NOT NULL,
+  `payterms` VARCHAR(12) NOT NULL,
+  `title_id` VARCHAR(6) NOT NULL,
+  PRIMARY KEY (`stor_id`, `ord_num`, `title_id`),
+  INDEX `sales_title_id` (`title_id` ASC) VISIBLE,
+  CONSTRAINT `sales_ibfk_1`
+    FOREIGN KEY (`stor_id`)
+    REFERENCES `publications`.`stores` (`stor_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `sales_ibfk_2`
+    FOREIGN KEY (`title_id`)
+    REFERENCES `publications`.`titles` (`title_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `publications`.`titleauthor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `publications`.`titleauthor` (
+  `au_id` VARCHAR(11) NOT NULL,
+  `title_id` VARCHAR(6) NOT NULL,
+  `au_ord` TINYINT NULL DEFAULT NULL,
+  `royaltyper` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`au_id`, `title_id`),
+  INDEX `titleauthor_title_id` (`title_id` ASC) VISIBLE,
+  CONSTRAINT `titleauthor_ibfk_1`
+    FOREIGN KEY (`title_id`)
+    REFERENCES `publications`.`titles` (`title_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `titleauthor_ibfk_2`
+    FOREIGN KEY (`au_id`)
+    REFERENCES `publications`.`authors` (`au_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
