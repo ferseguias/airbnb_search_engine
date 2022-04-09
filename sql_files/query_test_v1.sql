4277d083be28b7e6791fcad40ecfb2faa64afd75
ferseguias <ferseg@gmail.com>
2022-04-09 21:34:07 +0200

    600.000 reviews uploaded to sql

---
+++ sql_files/query_test_v1.sql
@@ -0,0 +1,17 @@
+USE airbnb;
+
+SELECT * FROM hosts;
+
+SELECT COUNT(idhosts) FROM hosts;
+
+SELECT * FROM neighborhoods;
+
+SELECT COUNT(idneighborhoods) FROM neighborhoods;
+
+SELECT * FROM listings;
+
+SELECT COUNT(idlistings) FROM listings;
+
+SELECT * FROM reviews;
+
+SELECT COUNT(idreviews) FROM reviews;
